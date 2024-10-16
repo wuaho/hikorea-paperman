40fecf3d52028ea8b3bf72bb7568ca2e374dcc9e
wuaho <juanjoserequenalama@gmail.com>
2024-10-16 19:28:49 +0900

    Update components

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { AlienRegistrationFlow } from "./components/alien-registration-flow";
 
 function App() {
   return (
-    <>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <AlienRegistrationFlow />
-    </>
+    </div>
   );
 }
 
