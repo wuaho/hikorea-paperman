84edabf09ad03a8aaf648f613345a34384a43b5c
wuaho <juanjoserequenalama@gmail.com>
2024-12-07 21:26:04 +0900

    Add utils method to convert signatureURLtoBlob

--- apps/frontend/src/lib/utils.ts
+++ apps/frontend/src/lib/utils.ts
@@ -9,3 +9,14 @@ export function cn(...inputs: ClassValue[]) {
 export function formatDate(date: Date): string {
   return format(date, 'yyyy-MM-dd');
 }
+
+export function signatureURLtoBlob(signatureURL: string) {
+  const byteString = atob(signatureURL.split(',')[1]);
+  const mimeString = signatureURL.split(',')[0].split(':')[1].split(';')[0];
+  const ab = new ArrayBuffer(byteString.length);
+  const ia = new Uint8Array(ab);
+  for (let i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+  return new Blob([ab], { type: mimeString });
+}
