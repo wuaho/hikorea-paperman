b047248273614079fd8157563419f1166a455d6d
wuaho <juanjoserequenalama@gmail.com>
2024-12-09 20:12:44 +0900

    Change color of sonner text to korea-blue

--- apps/frontend/src/components/ui/sonner.tsx
+++ apps/frontend/src/components/ui/sonner.tsx
@@ -13,7 +13,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       toastOptions={{
         classNames: {
           toast:
-            'group toast group-[.toaster]:bg-white group-[.toaster]:text-zinc-950 group-[.toaster]:border-zinc-200 group-[.toaster]:shadow-lg dark:group-[.toaster]:bg-zinc-950 dark:group-[.toaster]:text-zinc-50 dark:group-[.toaster]:border-zinc-800',
+            'group toast group-[.toaster]:bg-white group-[.toaster]:text-korea-blue group-[.toaster]:border-zinc-200 group-[.toaster]:shadow-lg dark:group-[.toaster]:bg-zinc-950 dark:group-[.toaster]:text-zinc-50 dark:group-[.toaster]:border-zinc-800',
           description:
             'group-[.toast]:text-zinc-500 dark:group-[.toast]:text-zinc-400',
           actionButton:
